591632a15d6a533159ff2a4610cbd209734e3f2c
Mayank2004 <133333626+Mayank2004-25@users.noreply.github.com>
2024-03-24 18:44:45 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -33,49 +33,104 @@ const projects = [
   },
 ];
 
-const AllButtons = document.querySelectorAll(".see-project");
-const work = document.querySelector(".work");
+const AllButtons = document.getElementsByClassName("see-project");
+const work = document.querySelector(".project");
 const popUp = document.createElement("section");
 popUp.classList.add("modal-container");
-
-function createModal(cardNumber) {
+function popUpDesktop(cardNumber) {
+  popUp.style.display = "block";
+  work.appendChild(popUp);
   popUp.innerHTML = `
     <div class="modal-content">
-      <div class="modal-header">
-        <h2>${projects[cardNumber].title}</h2>
+    <div class="modal-header">
+      <h2>${projects[cardNumber].title}</h2>
         <i class="fa fa-times" id="close-popUp" aria-hidden="true"></i>
-      </div>
-      <div class="modal-image">
-        <img src="${projects[cardNumber].src}" alt="App Image">
-      </div>
-      <div class="modal-description">
-        <p>${projects[cardNumber].description}</p>
-      </div>
-      <div class="modal-technologies">
-        <ul class="project-tools">
-          ${projects[cardNumber].technologies.map(tech => `<li><img src="images/Rectangle.png" alt="line" /></li><li>${tech}</li>`).join('')}
-        </ul>
-      </div>
-      <div class="modal-btns">
-        <button>
-          <a href="${projects[cardNumber].seeLiveLink}" target="_blank" rel="noopener noreferrer">See Live <img src="images/see-live-Icon.png" alt="See live"></a>
-        </button>
-        <button>
-          <a href="${projects[cardNumber].seeSourceLink}" target="_blank" rel="noopener noreferrer">See Source <img src="images/Github.png" alt="Github icon"></a>
-        </button>
-      </div>
     </div>
-  `;
+    <div class="modal-image">
+      <img src=${projects[cardNumber].src} alt="App Image">
+    </div>
+    <div class="modal-description">
+      <p>${projects[cardNumber].description}</p>
+    </div>
+    <div class="modal-technologies">
+      <ul class="project-tools">
+        <li><img src="images/Rectangle.png" alt="line" /></li>
+        <li>${projects[cardNumber].technologies[0]}</li>
+        <li><img src="images/Rectangle.png" alt="line" /></li>
+        <li>${projects[cardNumber].technologies[1]}</li>
+        <li><img src="images/Rectangle.png" alt="line" /></li>
+        <li>${projects[cardNumber].technologies[2]}</li>
+        <li><img src="images/Rectangle.png" alt="line" /></li>
+        <li>${projects[cardNumber].technologies[3]}</li>
+        <li><img src="images/Rectangle.png" alt="line" /></li>
+      </ul>
+    </div>
+    <div class="modal-btns">
+    <button>
+    <a href="${projects[cardNumber].seeLiveLink}">See Live <img src="images/see-live-Icon.png" alt="See live"></i></a>
+  </button>
+  <button>
+    <a href="${projects[cardNumber].seeSourceLink}">See Source <img src="images/Github.png" alt="Github icon"></a>
+  </button>
+    </div>
+  </div>
+  </section>`;
+  document.addEventListener("click", (event) => {
+    if (event.target.id === "close-popUp") {
+      popUp.style.display = "none";
+    }
+  });
+}
+function popUpMobile(cardNumber) {
+  popUp.style.display = "block";
   work.appendChild(popUp);
-
-  document.getElementById("close-popUp").addEventListener("click", () => {
-    popUp.style.display = "none";
+  popUp.innerHTML = `
+    <div class="modal-content">
+    <div class="modal-header">
+      <h2>${projects[cardNumber].title}</h2>
+        <i class="fa fa-times" id="close-popUp" aria-hidden="true"></i>
+    </div>
+    <div class="modal-image">
+      <img src=${projects[cardNumber].src} alt="App Image">
+    </div>
+    <div class="modal-description">
+      <p>${projects[cardNumber].description}</p>
+    </div>
+    <div class="modal-technologies">
+      <ul class="project-tools">
+        <li><img src="images/Rectangle.png" alt="line" /></li>
+        <li>${projects[cardNumber].technologies[0]}</li>
+        <li><img src="images/Rectangle.png" alt="line" /></li>
+        <li>${projects[cardNumber].technologies[1]}</li>
+        <li><img src="images/Rectangle.png" alt="line" /></li>
+        <li>${projects[cardNumber].technologies[2]}</li>
+        <li><img src="images/Rectangle.png" alt="line" /></li>
+        <li>${projects[cardNumber].technologies[3]}</li>
+        <li><img src="images/Rectangle.png" alt="line" /></li>
+      </ul>
+    </div>
+    <div class="modal-btns">
+    <button>
+    <a href="${projects[cardNumber].seeLiveLink}">See Live <img src="images/see-live-Icon.png" alt="See live"></i></a>
+  </button>
+  <button>
+    <a href="${projects[cardNumber].seeSourceLink}">See Source <img src="images/Github.png" alt="Github icon"></a>
+  </button>
+    </div>
+  </div>
+  </section>`;
+  document.addEventListener("click", (event) => {
+    if (event.target.id === "close-popUp") {
+      popUp.style.display = "none";
+    }
   });
 }
 
-AllButtons.forEach((button, index) => {
-  button.addEventListener("click", () => {
-    createModal(index);
-    popUp.style.display = "block";
+for (let i = 0; i < AllButtons.length; i += 1) {
+  AllButtons[i].addEventListener("click", () => {
+    popUpDesktop(i);
   });
-});
+  AllButtons[i].addEventListener("click", () => {
+    popUpMobile(i);
+  });
+}
